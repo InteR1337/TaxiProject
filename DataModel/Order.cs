92e2638d68e4199acca00bbabbee540014fdc5c3
Alexander Zheleznov <Alexander.Zheleznov@icl-services.com>
2016-08-23 12:30:03 +0300

    initial commit: Added some data models.

---
+++ DataModel/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class TaxiOrder
+    {
+        public int Id { get; set; }
+        public int PassengersNumber { get; set; }
+        public string Place { get; set; }
+        public Taxi TaxiUsed { get; set; }
+        public DateTime OrderedAt { get; set; }
+        public int TravelTime { get; set; }
+        public int Rating { get; set; }
+    }
+}
---
+++ DataModel/Taxi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class Taxi
+    {
+        public int Id { get; set; }
+        public string CarBrand { get; set; }
+        public string CarModel { get; set; }
+        public int PassengerSeats { get; set; }
+        public DateTime UsedFrom { get; set; }
+    }
+}
